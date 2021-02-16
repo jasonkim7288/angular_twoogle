8e8b01c9883e784d848b9c90db48f4f9f729bd5a
jasonkim7288 <jason.kim7288@gmail.com>
2021-02-16 11:42:56 +1000

    add auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from './user.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user$: Observable<User> = new Observable();
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
 
-  constructor() { }
+  async googleSignIn() {
+    
+  }
 }
